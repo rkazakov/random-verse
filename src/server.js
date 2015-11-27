80898bd67ee9127300a368747c0643b2fb2c5d58
Ruslan Kazakov <ruslan.kazakov@mi9.com.au>
2015-11-27 11:00:46 +1100

    small refactoring

--- src/server.js
+++ src/server.js
@@ -6,14 +6,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-console.log(__dirname);
-
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/images', express.static(__dirname + '/images'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Listening on port %s...', port);
 });
