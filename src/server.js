8d4feca4f19bd8dd957ff3df46310406ee0763f9
Ruslan Kazakov <ruslan.kazakov@mi9.com.au>
2015-11-30 13:04:13 +1100

    trying http request

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -13,6 +14,24 @@ app.use('/scripts', express.static(__dirname + '/scripts'));
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
+
+  // get
+  var optionsget = {
+    host : 'bibleapi-dev4christ.rhcloud.com', // here only the domain name
+    port : 80,
+    path : '/Gen1:1', // the rest of the url with parameters if needed
+    method : 'GET' // do GET
+  };
+
+  var reqGet = http.request(optionsget, function(res) {
+    res.on('data', function(d) {
+      process.stdout.write(d);
+    });
+  });
+  reqGet.end();
+  reqGet.on('error', function(e) {
+    console.error(e);
+  });
 
   console.log('Listening on port %s...', port);
 });
