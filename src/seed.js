a6ee75fa0beccf50cb09659a17f7d2f1fc97bd03
Ruslan Kazakov <ruslan.kazakov@mi9.com.au>
2015-11-30 21:22:51 +1100

    adding fake data, fixing routes

---
+++ src/seed.js
@@ -0,0 +1,27 @@
+var locallydb = require('locallydb');
+
+var db = new locallydb('_mockdb');
+var collection = db.collection('verses');
+
+if (collection.items.length === 0)
+{
+  collection.insert([
+    {reference: "Psalm 16:8"},
+    {reference: "Psalm 34:8"},
+    {reference: "Psalm 46:10"},
+    {reference: "Psalm 55:22"},
+    {reference: "Psalm 119:89"},
+    {reference: "Psalm 119:50"},
+    {reference: "Psalm 120:1"},
+    {reference: "Proverbs 3:5-6"},
+    {reference: "Proverbs 18:10"},
+    {reference: "Isaiah 26:3"},
+    {reference: "Isaiah 41:10"},
+    {reference: "John 14:27"},
+    {reference: "John 16:33"},
+    {reference: "Galatians 2:20"},
+    {reference: "Revelation 21:4"}
+  ]);
+}
+
+//console.log(collection.items.length);
--- src/server/router.js
+++ src/server/router.js
@@ -3,10 +3,14 @@ var bibleApi = require('./bibleApi');
 var router = module.exports = require('express').Router();
 
 router.get('/api/random/', function (req, res) {
-  var value = Math.floor(Math.random() * 10) + 1;
-  res.json({verse: value});
+  var locallydb = require('locallydb');
+  var db = new locallydb('_mockdb');
+  var collection = db.collection('verses');
 
-  /*bibleApi('Gen1:2', function(data) {
+  var random = Math.floor(Math.random() * 14);
+  var ref = collection.get(random).reference;
+
+  bibleApi(ref.replace(/ /g, "%20"), function(data) {
       res.send(JSON.parse(data));
-  });*/
+  });
 });
