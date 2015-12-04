76179fd3dba56c2d1abf375af3ffbe29219f2233
Ruslan Kazakov <ruslan.kazakov@mi9.com.au>
2015-12-04 16:05:38 +1100

    adding gulp task for stylus

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ var app = express();
 var router = require('./server/router');
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.use(router);
 
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/images', express.static(__dirname + '/images'));
-app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/', express.static(__dirname + '/public'));
+// app.use('/css', express.static(__dirname + '/css'));
+// app.use('/images', express.static(__dirname + '/images'));
+// app.use('/scripts', express.static(__dirname + '/scripts'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 
-gulp.task('browserify', function () {
-  gulp.src('src/main.js')
-    .pipe(plumber())
-    .pipe(browserify({ transform: 'reactify', debug: true }))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('public'));
+gulp.task('styles', function() {
+  gulp.src(['client/stylus/*.styl'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(stylus())
+    .pipe(gulp.dest('public/styles/'))
 });
 
-gulp.task('default', ['browserify']);
+// gulp.task('browserify', function () {
+//   gulp.src('src/main.js')
+//     .pipe(plumber())
+//     .pipe(browserify({ transform: 'reactify', debug: true }))
+//     .pipe(concat('main.js'))
+//     .pipe(gulp.dest('public'));
+// });
 
-gulp.task('watch', function () {
-  gulp.watch('src/**/*.*', ['default']);
+gulp.task('default', function() {
+  gulp.watch("client/stylus/*.styl", ['styles']);
 });
