7ba586a4bb0c4e7c3f0a78476b5e8333bd435bf5
Ruslan Kazakov <ruslan.kazakov@mi9.com.au>
2015-12-18 13:15:15 +1100

    fixing gulp live reload, adding nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,10 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     reactify = require('reactify'),
     uglify = require('gulp-uglify'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    nodemon = require('gulp-nodemon');
+
+var BROWSER_SYNC_RELOAD_DELAY = 500;
 
 gulp.task('styles', function() {
   gulp.src(['client/stylus/**/*.styl'])
@@ -50,18 +53,48 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('public/scripts/'))
 });
 
-gulp.task('server', function() {
-  browserSync({
-    server: {
-     baseDir: './public'
-    }
+gulp.task('nodemon', function(cb) {
+  var called = false;
+  return nodemon({
+    // nodemon our expressjs server
+    script: 'app.js',
+    // watch core server file(s) that require server restart on change
+    watch: ['app.js', 'server/**/*.js']
+  })
+  .on('start', function onStart() {
+    // ensure start only got called once
+    if (!called) { cb(); }
+    called = true;
+  })
+  .on('restart', function onRestart() {
+    // reload connected browsers after a slight delay
+    setTimeout(function reload() {
+      browserSync.reload({
+        stream: false
+      });
+    }, BROWSER_SYNC_RELOAD_DELAY);
   });
 });
 
-gulp.task('default', ['scripts', 'styles', 'server'], function() {
-  return gulp.watch([
-    "client/scripts/**/*.js", "client/stylus/**/*.styl"
-  ], [
-   'scripts', 'styles', browserSync.reload
-  ]);
+gulp.task('browser-sync', ['nodemon'], function() {
+  // for more browser-sync config options: http://www.browsersync.io/docs/options/
+  browserSync({
+    // informs browser-sync to proxy our expressjs app which would run at the following location
+    proxy: 'http://localhost:3000',
+    // informs browser-sync to use the following port for the proxied app
+    // notice that the default port is 3000, which would clash with our expressjs
+    port: 4000,
+    // open the proxied app in chrome
+    browser: 'google chrome'
+  });
+});
+
+gulp.task('bs-reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync'], function() {
+  gulp.watch('client/scripts/**/*.js', ['scripts', browserSync.reload]);
+  gulp.watch('client/stylus/**/*.styl',  ['styles', browserSync.reload]);
+  gulp.watch('public/**/*.html', ['bs-reload']);
 });
